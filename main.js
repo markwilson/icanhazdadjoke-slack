9b018846ce5a3d8cd55c3a6a32403300a75537db
Mark Wilson <markwilson@users.noreply.github.com>
2021-08-12 16:29:57 +0100

    Try different user agent for dad joke API 503 errors

--- main.js
+++ main.js
@@ -6,9 +6,16 @@ axios
   .get("https://icanhazdadjoke.com/", {
     headers: {
       Accept: "application/json",
+      "User-Agent": "Mozilla/5.0",
     },
   })
   .then(({ data }) => {
     axios.post(slackUrl, data);
   })
-  .catch(console.error);
+  .catch((error) => {
+    console.error(error);
+
+    axios.post(slackUrl, {
+      joke: "Dad joke API failed... Make up your own jokes. Maybe a knock-knock joke?",
+    });
+  });
